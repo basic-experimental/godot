f246327f79f07eaafb338d908d0bc483b70a7bc3
basic-experimental <das904@icloud.com>
2026-09-03 13:53:02 -0400

    Bumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,11 @@ extends CharacterBody2D
 const SFX_JUMP = preload("res://assets/sounds/GoatJumpsfx.wav")
 const SFX_WALK = preload("res://assets/sounds/GoatWalksfx.wav")
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const ACCELERATION = 300.0
+const JUMP_VELOCITY = -350.0
+const BUMP_VELOCITY = -150.0
+const BUMP_MULT = -0.5
+const CHARGE_MULT = 2.0
 
 enum PowerUp {
 	Normal,
@@ -14,7 +18,9 @@ enum PowerUp {
 	Fire	
 }
 
-var power_up: PowerUp = PowerUp.Normal 
+var power_up: PowerUp = PowerUp.Normal
+var is_jumping = false 
+var is_charging = false
 
 func _physics_process(delta: float) -> void:
 	# --- MOVEMENT ---
@@ -24,17 +30,24 @@ func _physics_process(delta: float) -> void:
 	
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
 		velocity.y = JUMP_VELOCITY
 		play_player_sound(SFX_JUMP)
-		
-	var speed_mult = 1
-	if Input.is_action_pressed("charge"):
-		speed_mult *= 2
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0 and is_jumping:
+		is_jumping = false
+		velocity.y /= 2
+	
+	if Input.is_action_pressed("charge") and is_on_floor():
+		is_charging = true
+	else:
+		is_charging = false
 		
 	# Get the input direction and handle the movement/deceleration
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED * speed_mult
+		var speed = SPEED * CHARGE_MULT if is_charging else SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, ACCELERATION * delta)
 		# Only play walk sound if on floor and not already playing
 		if is_on_floor() and not player_sfx.is_playing():
 			play_player_sound(SFX_WALK)
@@ -59,7 +72,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play("run")
 
+	var vx_before_collide = velocity.x
+	
 	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var normal = collision.get_normal()
+		# Bump on wall
+		if abs(normal.x) > abs(normal.y) and is_charging and abs(vx_before_collide) > SPEED:
+			velocity.x = BUMP_MULT * vx_before_collide
+			
+			if is_on_floor():
+				velocity.y = BUMP_VELOCITY
 	
 func play_player_sound(stream: AudioStream):
 	player_sfx.stream = stream
