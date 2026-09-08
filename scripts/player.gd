a52748d84c1525485cfb812d9fbc504ffe3cdfd4
basic-experimental <das904@icloud.com>
2026-09-08 15:53:38 -0400

    Add slam powerup and start work on rocket powerup

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,9 @@ const JUMP_VELOCITY = -350.0
 const BUMP_VELOCITY = -150.0
 const BUMP_MULT = -0.5
 const CHARGE_MULT = 2.0
+const BUMP_SPEED_MULT_THRESHOLD = 1.5
+const SLAM_SPEED = 400.0
+const ROCKET_TIME = 1000.0
 
 enum PowerUp {
 	Normal,
@@ -19,19 +22,26 @@ enum PowerUp {
 }
 
 var power_up: PowerUp = PowerUp.Normal
+var direction = 0.0
 var is_jumping = false 
 var is_charging = false
+var is_slamming = false
+var is_rocketing = false
+var rocket_start_time = -9223372036854775808 # Minimum int value
 
 func _physics_process(delta: float) -> void:
 	# --- MOVEMENT ---
 	# Gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if is_slamming:
+			velocity.y = SLAM_SPEED
+		else:
+			velocity += get_gravity() * delta
 	
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		is_jumping = true
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * (1 + abs(velocity.x / 1000))
 		play_player_sound(SFX_JUMP)
 	
 	if Input.is_action_just_released("jump") and velocity.y < 0 and is_jumping:
@@ -43,19 +53,37 @@ func _physics_process(delta: float) -> void:
 	else:
 		is_charging = false
 		
+	# Slam
+	if power_up == PowerUp.Slam:
+		if  !is_slamming && !is_on_floor() && Input.is_action_just_pressed("slam"):
+			is_slamming = true
+		elif is_slamming && is_on_floor() && Input.is_action_just_released("slam"):
+			is_slamming = false
+	
+	# Rocket
+	if power_up == PowerUp.Rocket:
+		if !is_rocketing && !is_on_floor() && Input.is_action_just_pressed("charge"):
+			is_rocketing = true
+			rocket_start_time = Time.get_ticks_msec()
+		elif is_rocketing && Input.is_action_just_released("rocket"):
+			is_rocketing = false
+		elif is_rocketing && Time.get_ticks_msec() - rocket_start_time >= ROCKET_TIME:
+			is_rocketing = false
+	
 	# Get the input direction and handle the movement/deceleration
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		var speed = SPEED * CHARGE_MULT if is_charging else SPEED
-		velocity.x = move_toward(velocity.x, direction * speed, ACCELERATION * delta)
-		# Only play walk sound if on floor and not already playing
-		if is_on_floor() and not player_sfx.is_playing():
-			play_player_sound(SFX_WALK)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		# Stop walk sound when player stops moving on the ground
-		if is_on_floor() and player_sfx.stream == SFX_WALK:
-			player_sfx.stop()
+	if !is_slamming:
+		direction = Input.get_axis("move_left", "move_right")
+		if direction:
+			var speed = SPEED * CHARGE_MULT if is_charging else SPEED
+			velocity.x = move_toward(velocity.x, direction * speed, ACCELERATION * delta)
+			# Only play walk sound if on floor and not already playing
+			if is_on_floor() and not player_sfx.is_playing():
+				play_player_sound(SFX_WALK)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			# Stop walk sound when player stops moving on the ground
+			if is_on_floor() and player_sfx.stream == SFX_WALK:
+				player_sfx.stop()
 	
 	# --- GRAPHICS ---
 	# Sprite flipping
@@ -65,7 +93,9 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 	
 	# Animation
-	if !is_on_floor():
+	if is_slamming:
+		animated_sprite.play("slam")
+	elif !is_on_floor():
 		animated_sprite.play("jump")
 	elif direction == 0:
 		animated_sprite.play("idle")
@@ -80,7 +110,7 @@ func _physics_process(delta: float) -> void:
 		var collision = get_slide_collision(i)
 		var normal = collision.get_normal()
 		# Bump on wall
-		if abs(normal.x) > abs(normal.y) and is_charging and abs(vx_before_collide) > SPEED:
+		if abs(normal.x) > abs(normal.y) and is_charging and abs(vx_before_collide) > SPEED * BUMP_SPEED_MULT_THRESHOLD:
 			velocity.x = BUMP_MULT * vx_before_collide
 			
 			if is_on_floor():
