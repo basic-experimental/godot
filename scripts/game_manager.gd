247c0eae2a5194b55606d5e46f4d7fd77b02d3bf
josephdangelo17 <135060368+josephdangelo17@users.noreply.github.com>
2026-09-20 20:02:01 -0400

    Added opening cutscene

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,12 +1,19 @@
 extends Node
 @onready var cutscene = $Cutscene
-@onready var StartCutscene = $StartCutscene
 @onready var player: CharacterBody2D = $"/root/Game/Player"
 @onready var camera = $Path2D/PathFollow2D/CutsceneCamera
 @onready var gompei = $GompeiWalk
-@onready var startcamera = $Path2D2/PathFollow2D/StartCutsceneCamera
+@onready var start_cutscene = $/root/Game/OpeningCutscene/StartCutscene
+
 var is_cutscene = false
 var player_entered_area = false
+var start_cutscene_played = false
+
+func _ready() -> void:
+	if !start_cutscene_played:
+		start_cutscene.play("StartAnimation")
+		start_cutscene_played = true
+
 
 func _on_cutscene_detection_body_entered(body: Node2D) -> void:
 	print("Body entered: ", body.name, " | Groups: ", body.get_groups())
@@ -27,22 +34,3 @@ func rocket_cutscene():
 	cutscene.play("cutscene")
 	await get_tree().create_timer(12.0).timeout
 	get_tree().change_scene_to_file("res://scenes/world_2.tscn")
-
-
-func _on_start_cutscene_detection_body_entered(body: Node2D) -> void:
-	print("Body entered: ", body.name, " | Groups: ", body.get_groups())
-	if body == player:
-		if !player_entered_area:
-			player_entered_area = true
-			start_cutscene()
-			
-func start_cutscene():
-	print("start_cutscene triggered")
-	gompei.set_visibility_layer_bit(1,true)
-	is_cutscene = true
-	player.set_physics_process(false)
-	player.velocity = Vector2.ZERO
-	player.visible = false
-	player.get_node("Camera2D").enabled = false
-	startcamera.enabled = true
-	cutscene.play("StartCutscene")
